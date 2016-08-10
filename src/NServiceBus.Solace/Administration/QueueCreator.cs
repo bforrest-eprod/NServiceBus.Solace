b16ab5d02fea4f993aa2fd92997733b3cd31fc81
Eric Burcham <jameseburcham@gmail.com>
2016-08-09 21:47:43 -0500

    Got a basic project created, libraries referenced, and a BS class done to force a valid parse of the Solace DLL on build.  Good to go for Barry.

---
+++ src/NServiceBus.Solace/Administration/QueueCreator.cs
@@ -0,0 +1,58 @@
+﻿namespace NServiceBus.Transports.Solace.Administration
+{
+    using System;
+    using System.Threading.Tasks;
+    using SolaceSystems.Solclient.Messaging;
+    using Transports;
+
+    internal class QueueCreator : ICreateQueues
+    {
+        public Task CreateQueueIfNecessary(QueueBindings queueBindings, string identity)
+        {
+            foreach (var receivingAddress in queueBindings.ReceivingAddresses)
+            {
+                CreateQueueIfNecessary(receivingAddress);
+            }
+
+            foreach (var sendingAddress in queueBindings.SendingAddresses)
+            {
+                CreateQueueIfNecessary(sendingAddress);
+            }
+
+            return TaskEx.CompletedTask;
+        }
+
+        private void CreateQueueIfNecessary(string address)
+        {
+            throw new NotImplementedException();
+
+            // *************************************************************************************
+            // TL;DR - You should delete all but the first line of this method.
+            // *************************************************************************************
+            // This is some basic junk copied out of the Solace .Net Client Examples.
+            // It doesn't do anything functional, and I'm not even sure this is the right approach.
+            // I just wanted to reference some crap to force parsing of the referenced
+            // SolaceSystems.Solclient.Messaging assembly during compilation.
+            ContextFactory.Instance.Init(new ContextFactoryProperties());
+            var context = ContextFactory.Instance.CreateContext(new ContextProperties(), null);
+
+            using (var session = CreateSession(context))
+            {
+                if (session.Connect() == ReturnCode.SOLCLIENT_OK)
+                {
+                    Console.WriteLine("Session successfully connected");
+                }
+            }
+        }
+
+        private static ISession CreateSession(IContext context)
+        {
+            // *************************************************************************************
+            // YOU SHOULD DELETE THIS METHOD.  IT ONLY EXISTS TO LET THE CALLER COMPILE.
+            // *************************************************************************************
+            return context.CreateSession(new SessionProperties(),
+                (sender, args) => { },
+                (sender, args) => { });
+        }
+    }
+}
---
+++ src/NServiceBus.Solace/TaskEx.cs
@@ -0,0 +1,10 @@
+﻿namespace NServiceBus.Transports.Solace
+{
+    using System.Threading.Tasks;
+
+    internal static class TaskEx
+    {
+        //TODO: remove when we update to 4.6 and can use Task.CompletedTask
+        public static readonly Task CompletedTask = Task.FromResult(0);
+    }
+}
